7b4b6b919426ef3e1e9fa2bc230244fcd514fa2d
Ladan Nasserian <ladanazita@users.noreply.github.com>
2017-09-22 18:09:05 -0700

    Remove pods (#26)

--- Example/Pods/Headers/Public/Segment-Amplitude/SEGAmplitudeIntegration.h
+++
@@ -1,1 +0,0 @@
-../../../../../Pod/Classes/SEGAmplitudeIntegration.h
--- Example/Pods/Target Support Files/Pods-Segment-Amplitude_Example/Pods-Segment-Amplitude_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Segment_Amplitude_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Segment_Amplitude_Example
-@end
