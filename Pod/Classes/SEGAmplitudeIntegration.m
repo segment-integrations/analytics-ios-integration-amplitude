c583131dfb71e29f6ba9bed6cd1d91334ea512d0
Cody Garvin <emig647@gmail.com>
2020-11-10 08:32:55 -0800

    Updated header import

--- Pod/Classes/SEGAmplitudeIntegration.m
+++ Pod/Classes/SEGAmplitudeIntegration.m
@@ -1,9 +1,10 @@
 #import "SEGAmplitudeIntegration.h"
-//#if defined(__has_include) && __has_include(<Analytics/Analytics.h>)
-//#import <Analytics/Analytics.h>
-//#else
+#if defined(__has_include) && __has_include(<Analytics/Analytics.h>)
+#import <Analytics/Analytics.h>
+#import <Analytics/SEGAnalyticsUtils.h>
+#else
 #import <Segment/Segment.h>
-//#endif
+#endif
 
 
 @implementation SEGAmplitudeIntegration
