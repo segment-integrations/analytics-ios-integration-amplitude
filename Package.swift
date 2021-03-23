df9c2d2069219e0795382f7510bccfbcad181ba4
calube <hello.calebdavis@gmail.com>
2021-03-23 09:32:19 -0500

    update package name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "analytics-ios-integration-amplitude",
+    name: "Segment-Amplitude",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "analytics-ios-integration-amplitude",
-            targets: ["analytics-ios-integration-amplitude"]),
+            name: "Segment-Amplitude",
+            targets: ["Segment-Amplitude"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "analytics-ios-integration-amplitude",
+            name: "Segment-Amplitude",
             dependencies: []),
         .testTarget(
-            name: "analytics-ios-integration-amplitudeTests",
-            dependencies: ["analytics-ios-integration-amplitude"]),
+            name: "Segment-AmplitudeTests",
+            dependencies: ["Segment-Amplitude"]),
     ]
 )
